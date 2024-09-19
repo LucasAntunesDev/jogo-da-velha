7588f0000d004510e1b269a731db7b8900ac31f8
Lucas Antunes <lucasantuneslara06@gmail.com>
2024-09-19 07:44:42 -0300

    Mantendo estado do jogo no localhost

--- src/components/Tabuleiro.jsx
+++ src/components/Tabuleiro.jsx
@@ -1,11 +1,13 @@
 import Casa from './Casa'
 
-const Tabuleiro = ({casas, casasVitoria, inserirValor}) => {
+const Tabuleiro = ({casas, casasVitoria, handleClick}) => {
+  const safeCasas = Array.isArray(casas) ? casas : Array(9).fill(null)
+
   return (
-    <div className="grid grid-cols-3 grid-rows-3 *:border-2 *:bg-slate-800 gap-1 *:rounded-xl text-zinc-50 mx-auto h-72 w-7/12 max-h-screen bg-slate-900 rounded shadow-2xl">
-      {casas.map((casa, index) => (
+    <div className="grid grid-cols-3 grid-rows-3 gap-1 rounded-xl text-zinc-50 mx-auto h-72 w-7/12 max-h-screen bg-slate-900 shadow-2xl text-3xl">
+      {safeCasas.map((casa, index) => (
         <Casa
-          onClick={() => inserirValor(index)}
+          onClick={() => handleClick(index)}
           valor={casa}
           key={index}
           id={index}
