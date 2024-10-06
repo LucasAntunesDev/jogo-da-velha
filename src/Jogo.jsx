a461d5efa8ad39777c8eb956a2ee3fa90b32a8af
Lucas Antunes <lucasantuneslara06@gmail.com>
2024-10-06 15:27:55 -0300

    Fix: casas do localstorage sendo armazenadas nos vetores de jogadas

--- src/Jogo.jsx
+++ src/Jogo.jsx
@@ -1,9 +1,7 @@
-import { useState, useEffect } from 'react';
-import { ArrowRightIcon } from '@heroicons/react/16/solid';
-import O from './components/O';
-import X from './components/X';
-import Tabuleiro from './components/Tabuleiro';
-import Modal from './components/Modal';
+import {useState} from 'react'
+import {ArrowRightIcon} from '@heroicons/react/16/solid'
+import Tabuleiro from './components/Tabuleiro'
+import Modal from './components/Modal'
 
 const Jogo = () => {
   const combinacoesVitoria = [
@@ -15,197 +13,219 @@ const Jogo = () => {
     [2, 5, 8], // Vertical
     [0, 4, 8],
     [6, 4, 2], // Diagonais
-  ];
+  ]
 
+  const [resultado, setResultado] = useState(null)
+  const [casasVitoria, setCasasVitoria] = useState([])
+  const [jogadorAtual, setJogadorAtual] = useState(0)
+  const [jogadasJogador1, setJogadasJogador1] = useState([])
+  const [jogadasJogador2, setJogadasJogador2] = useState([])
+  const [desativado, setDesativado] = useState(false)
+
+  //Casas
   const [casas, setCasas] = useState(() => {
-    const savedCasas = localStorage.getItem('casas');
-    if (savedCasas) {
+    const casasLocalStorage = localStorage.getItem('casas')
+    if (casasLocalStorage) {
       try {
-        return JSON.parse(savedCasas);
+        const casasSalvas = JSON.parse(casasLocalStorage)
+
+        const jogadasJ1 = casasSalvas
+          .map((item, index) => (item === 'X' ? index : -1))
+          .filter(index => index !== -1)
+        const jogadasJ2 = casasSalvas
+          .map((item, index) => (item === 'O' ? index : -1))
+          .filter(index => index !== -1)
+
+        setJogadasJogador1(jogadasJ1)
+        setJogadasJogador2(jogadasJ2)
+
+        return casasSalvas
       } catch (error) {
-        console.error('Erro ao analisar casas do localStorage:', error);
-        return Array(9).fill(null); // Retorna um array padrão em caso de erro
+        console.error('Erro ao analisar casas do localStorage:', error)
+        return Array(9).fill(null)
       }
     }
-    return Array(9).fill(null);
-  });
-  
-  const [resultado, setResultado] = useState(null);
-  const [casasVitoria, setCasasVitoria] = useState([]);
-  const [jogadorAtual, setJogadorAtual] = useState(0);
-  const [jogadasJogador1, setJogadasJogador1] = useState([]);
-  const [jogadasJogador2, setJogadasJogador2] = useState([]);
-  const [desativado, setDesativado] = useState(false);
-  const [trancarDificuldade, setTrancarDificuldade] = useState(localStorage.getItem('trancar_dificuldade') || false);
-  const [dificuldade, setDificuldade] = useState(localStorage.getItem('dificuldade') || 'fácil');
-  const [hidden, setHidden] = useState(true);
+    return Array(9).fill(null)
+  })
 
-  const handleDificuldade = (dificuldade) => {
-    setDificuldade(dificuldade);
-    localStorage.setItem('dificuldade', dificuldade);
-  };
+  const [trancarDificuldade, setTrancarDificuldade] = useState(
+    localStorage.getItem('trancar_dificuldade') || false
+  )
+  const [dificuldade, setDificuldade] = useState(
+    localStorage.getItem('dificuldade') || 'fácil'
+  )
+  const [hidden, setHidden] = useState(true)
+
+  const handleDificuldade = dificuldade => {
+    setDificuldade(dificuldade)
+    localStorage.setItem('dificuldade', dificuldade)
+  }
 
   const handleTrancarDificuldade = () => {
-    setTrancarDificuldade(true);
-    localStorage.setItem('trancar_dificuldade', true);
-  };
+    setTrancarDificuldade(true)
+    localStorage.setItem('trancar_dificuldade', true)
+  }
 
-  const atualizarLocalStorage = (resultado) => {
+  const atualizarLocalStorage = resultado => {
     const resultados = JSON.parse(localStorage.getItem('resultados')) || {
       jogador1: 0,
       jogador2: 0,
       empates: 0,
-    };
+    }
 
     if (resultado === 'Jogador') {
-      resultados.jogador1 += 1;
+      resultados.jogador1 += 1
     } else if (resultado === 'Máquina') {
-      resultados.jogador2 += 1;
+      resultados.jogador2 += 1
     } else if (resultado === 'Empate') {
-      resultados.empates += 1;
+      resultados.empates += 1
     }
 
-    localStorage.setItem('resultados', JSON.stringify(resultados));
-  };
+    localStorage.setItem('resultados', JSON.stringify(resultados))
+  }
 
-  const verificarVitoria = (jogadas) => {
+  const verificarVitoria = jogadas => {
     for (let combinacao of combinacoesVitoria) {
       if (combinacao.every(index => jogadas.includes(index))) {
-        setCasasVitoria(combinacao);
-        return true;
+        setCasasVitoria(combinacao)
+        return true
       }
     }
-    return false;
-  };
+    return false
+  }
 
-  const verificarEmpate = (novasCasas) => novasCasas.every(casa => casa !== null);
+  const verificarEmpate = novasCasas => novasCasas.every(casa => casa !== null)
 
   const reiniciarJogo = () => {
-    setCasas(Array(9).fill(null));
-    setJogadasJogador1([]);
-    setJogadasJogador2([]);
-    setJogadorAtual(0);
-    setResultado(null);
-    setCasasVitoria([]);
-    setDesativado(false);
-    setHidden(true);
-    setTrancarDificuldade(false);
-    localStorage.removeItem('trancar_dificuldade');
-    localStorage.removeItem('dificuldade');
-    localStorage.removeItem('casas');
-  };
+    setCasas(Array(9).fill(null))
+    setJogadasJogador1([])
+    setJogadasJogador2([])
+    setJogadorAtual(0)
+    setResultado(null)
+    setCasasVitoria([])
+    setDesativado(false)
+    setHidden(true)
+    setTrancarDificuldade(false)
+    localStorage.removeItem('trancar_dificuldade')
+    localStorage.removeItem('dificuldade')
+    localStorage.removeItem('casas')
+  }
 
-  const jogadaAleatoria = (novasCasas) => {
-    let indexAleatorio;
+  const jogadaAleatoria = novasCasas => {
+    let indexAleatorio
     do {
-      indexAleatorio = Math.floor(Math.random() * 9);
-    } while (novasCasas[indexAleatorio] !== null);
-    return indexAleatorio;
-  };
+      indexAleatorio = Math.floor(Math.random() * 9)
+    } while (novasCasas[indexAleatorio] !== null)
+    return indexAleatorio
+  }
 
   const bloquear = (verificador, novasCasas) => {
     for (let combinacao of combinacoesVitoria) {
-      const [a, b, c] = combinacao;
+      const [a, b, c] = combinacao
       const [primeiro, segundo, terceiro] = [
         novasCasas[a],
         novasCasas[b],
         novasCasas[c],
-      ];
+      ]
 
       if (
-        (primeiro === verificador && segundo === verificador && terceiro === null) ||
-        (primeiro === verificador && terceiro === verificador && segundo === null) ||
-        (segundo === verificador && terceiro === verificador && primeiro === null)
+        (primeiro === verificador &&
+          segundo === verificador &&
+          terceiro === null) ||
+        (primeiro === verificador &&
+          terceiro === verificador &&
+          segundo === null) ||
+        (segundo === verificador &&
+          terceiro === verificador &&
+          primeiro === null)
       ) {
-        return combinacao.find(index => novasCasas[index] === null);
+        return combinacao.find(index => novasCasas[index] === null)
       }
     }
-    return null;
-  };
+    return null
+  }
 
-  const jogadaDificil = (novasCasas) => {
-    const jogadaGanhar = bloquear('O', novasCasas);
+  const jogadaDificil = novasCasas => {
+    const jogadaGanhar = bloquear('O', novasCasas)
     if (jogadaGanhar !== null) {
-      return jogadaGanhar;
+      return jogadaGanhar
     }
 
-    const jogadaBloquear = bloquear('X', novasCasas);
+    const jogadaBloquear = bloquear('X', novasCasas)
     if (jogadaBloquear !== null) {
-      return jogadaBloquear;
+      return jogadaBloquear
     }
 
-    return jogadaAleatoria(novasCasas);
-  };
+    return jogadaAleatoria(novasCasas)
+  }
 
-  const handleClick = (index) => {
-    const novasCasas = casas.slice();
+  const handleClick = index => {
+    const novasCasas = casas.slice()
 
     if (novasCasas[index] === null && !resultado && !desativado) {
       if (jogadorAtual === 0) {
-        novasCasas[index] = 'X';
-        localStorage.setItem('casas', JSON.stringify(novasCasas));
-        const novasJogadasJogador1 = [...jogadasJogador1, index];
-        setJogadasJogador1(novasJogadasJogador1);
-        handleTrancarDificuldade();
-        setCasas(novasCasas);
+        novasCasas[index] = 'X'
+        localStorage.setItem('casas', JSON.stringify(novasCasas))
+        const novasJogadasJogador1 = [...jogadasJogador1, index]
+        setJogadasJogador1(novasJogadasJogador1)
+        handleTrancarDificuldade()
+        setCasas(novasCasas)
 
-        if (novasJogadasJogador1.length >= 3 && verificarVitoria(novasJogadasJogador1)) {
-          setResultado('Jogador');
-          atualizarLocalStorage('Jogador');
-          setHidden(false);
-          return;
+        if (
+          novasJogadasJogador1.length >= 3 &&
+          verificarVitoria(novasJogadasJogador1)
+        ) {
+          setResultado('Jogador')
+          atualizarLocalStorage('Jogador')
+          setHidden(false)
+          return
         }
 
         if (verificarEmpate(novasCasas)) {
-          setResultado('Empate');
-          atualizarLocalStorage('Empate');
-          setHidden(false);
-          return;
+          setResultado('Empate')
+          atualizarLocalStorage('Empate')
+          setHidden(false)
+          return
         }
 
-        setJogadorAtual(1);
-        setDesativado(true);
+        setJogadorAtual(1)
+        setDesativado(true)
 
         setTimeout(() => {
-          const indexMaquina = dificuldade === 'fácil'
-            ? jogadaAleatoria(novasCasas)
-            : jogadaDificil(novasCasas);
+          const indexMaquina =
+            dificuldade === 'fácil'
+              ? jogadaAleatoria(novasCasas)
+              : jogadaDificil(novasCasas)
 
-          novasCasas[indexMaquina] = 'O';
-          localStorage.setItem('casas', JSON.stringify(novasCasas));
-          const novasJogadasJogador2 = [...jogadasJogador2, indexMaquina];
-          setJogadasJogador2(novasJogadasJogador2);
-          setCasas(novasCasas);
+          novasCasas[indexMaquina] = 'O'
+          localStorage.setItem('casas', JSON.stringify(novasCasas))
+          const novasJogadasJogador2 = [...jogadasJogador2, indexMaquina]
+          setJogadasJogador2(novasJogadasJogador2)
+          setCasas(novasCasas)
 
-          if (novasJogadasJogador2.length >= 3 && verificarVitoria(novasJogadasJogador2)) {
-            setResultado('Máquina');
-            atualizarLocalStorage('Máquina');
-            setHidden(false);
-            return;
+          if (
+            novasJogadasJogador2.length >= 3 &&
+            verificarVitoria(novasJogadasJogador2)
+          ) {
+            setResultado('Máquina')
+            atualizarLocalStorage('Máquina')
+            setHidden(false)
+            return
           }
 
           if (verificarEmpate(novasCasas)) {
-            setResultado('Empate');
-            atualizarLocalStorage('Empate');
-            setHidden(false);
-            return;
+            setResultado('Empate')
+            atualizarLocalStorage('Empate')
+            setHidden(false)
+            return
           }
 
-          setJogadorAtual(0);
-          setDesativado(false);
-        }, 500);
+          setJogadorAtual(0)
+          setDesativado(false)
+        }, 500)
       }
     }
-  };
-
-  useEffect(() => {
-    const resultados = JSON.parse(localStorage.getItem('resultados')) || {
-      jogador1: 0,
-      jogador2: 0,
-      empates: 0,
-    };
-    console.log(resultados);
-  }, []);
+  }
 
   return (
     <>
@@ -266,7 +286,7 @@ const Jogo = () => {
         handleClick={handleClick}
       />
     </>
-  );
-};
+  )
+}
 
-export default Jogo;
+export default Jogo
